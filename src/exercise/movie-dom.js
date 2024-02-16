a2b92b0857f55f71895c00810d69166efe93a885
josepeee <joseantonioplaza777@gmail.com>
2024-02-16 20:31:18 +0100

    ejerci en clase...

---
+++ src/exercise/movie-dom.js
@@ -0,0 +1,53 @@
+
+import { movies } from "../data";
+
+function createMovieElement(movieData) {
+
+    //title 
+    const titleElement = document.createElement("div");
+    titleElement.classList = "movie-title";
+    titleElement.textContent = movieData.title;
+
+    //Description 
+    const descriptionElement = document.createElement("div");
+    descriptionElement.classList = "movie-description";
+    descriptionElement.textContent = movieData.description ;
+
+    //contet 
+
+    const contentElement = document.createElement("div");
+    contentElement.classList = "movie-content";
+    contentElement.appendChild(titleElement);
+    contentElement.appendChild(descriptionElement);
+
+    //Movie Grid
+
+    const movieElement = document.createElement("div");
+    movieElement.classList = "movie-grid";
+    movieElement.appendChild(contentElement);
+
+
+    return movieElement
+}
+const appElement = document.getElementById('app');
+
+movies.forEach((movieData) => {
+    const newMovieElement = createMovieElement(movieData);
+    appElement.appendChild(newMovieElement);
+})
+
+function clik(){
+ const lorem = document.querySelector(".lorem");
+
+ lorem.toggleAttribute("hidden");
+}
+const eventsElement = document.querySelector(".button");
+eventsElement.addEventListener("click" ,clik);
+//_____________________________________________//
+
+const h1 = document.querySelector("h1");
+const input = document.querySelector("input")
+
+input.addEventListener('keyup' , () =>{
+    h1.textContent = input.value;
+});
