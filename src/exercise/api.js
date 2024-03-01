8a32d1014a0f797d69af7e14482661ad3189be58
josepeee <joseantonioplaza3777@gmail.com>
2024-03-01 14:52:20 +0100

    en casa 01/03/24

--- src/exercise/api.js
+++ src/exercise/api.js
@@ -1,2 +1,19 @@
 
 
+console. clear();
+
+const apikey ="15d3ea6d0dc1d476efbca3eba2b9bbfb";
+const langIso = "es-Es";
+ const baseUrl= "https://api.themoviedb.org/3/";
+
+ const movieListUrl = `${baseUrl}/movie/now_playing?langue=${langIso}&api_key=${apikey}`;
+
+ fetch(movieListUrl)
+ .then((response) => response.json())
+ .then((response) => {
+    response?.results?.forEach((movieData) => {
+        const { id, title, overview}= movieData;
+        console.log({id, title, overview });
+    });
+ })
+ .catch((error) => console.error(error.message));
