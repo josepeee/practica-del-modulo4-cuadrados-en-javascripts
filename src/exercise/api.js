c0a648c5ed6527e5ceae457da850bf2d8a74a483
josepeee <joseantonioplaza3777@gmail.com>
2024-03-03 23:00:15 +0100

    ejerccio de la api.. restificado desde la casa 03/03/24

--- src/exercise/api.js
+++ src/exercise/api.js
@@ -6,14 +6,19 @@ const apikey ="15d3ea6d0dc1d476efbca3eba2b9bbfb";
 const langIso = "es-Es";
  const baseUrl= "https://api.themoviedb.org/3/";
 
- const movieListUrl = `${baseUrl}/movie/now_playing?langue=${langIso}&api_key=${apikey}`;
 
- fetch(movieListUrl)
- .then((response) => response.json())
- .then((response) => {
-    response?.results?.forEach((movieData) => {
-        const { id, title, overview}= movieData;
-        console.log({id, title, overview });
-    });
- })
- .catch((error) => console.error(error.message));
+async function getNowPlayingData(page = 1){
+    const movieListUrl = `${baseUrl}/movie/now_playing?langue=${langIso}&api_key=${apikey}&page=${page} `;
+     return await fetch(movieListUrl) 
+       .then((response) => response.json())
+       .then((data) => data?.results);
+}
+
+
+ getNowPlayingData().then((movies) => {
+    console.log(movies);
+ });
+
+
+
+//para el trabajo hacemos funciones que queremos hacer y despues las llamamos donde las queramos utilizar y asi podemos trabajar con mayor eficacia...
